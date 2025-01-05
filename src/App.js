867f488f8c7dcaec2e4ae4c33dd3cd928d259327
Yashashvi Maurya <yashashvimaurya@gmail.com>
2025-01-06 00:29:37 +0530

    step_4 instruction done

--- src/App.js
+++ src/App.js
@@ -1,46 +1,45 @@
-import './categories.styles.scss';
+import Directory from './components/directory/directory.component';
 
-// install sass
-// npm i sass --save-dev
-// https://sass-lang.com/guide/
 const App = () => {
+
+  // Great job karishma 🥳
+  // You are finally here 
+  // okay so ab karenge routing 
+  // whats is routing? 
+  // React Router DOM is a popular library in the React ecosystem that enables routing in web applications. It allows developers to manage and navigate between different views or pages in a React application while maintaining a single-page application (SPA) architecture. This means the browser doesn't need to reload the entire page when navigating, creating a seamless and dynamic user experience.
+  // npm i react-router-dom@6
+  // and now using the documentation ek route banao '/' 
+  // and then ek folder routes ke nam se and then usme ek folder home ke name se and usme ek file home.jsx usme ye sara code utha ke patak do and use '/' path pe render karao
+  // okay see you in next step 5 😁
   const categories = [
     {
       id: 1,
-      title: 'Hats',
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
-      title: 'Jackets',
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
-      title: 'Sneakers',
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
-      title: 'Womens',
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
-      title: 'Mens',
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
 
-  // one main thing now App.js hamesh neat and clean and kam code hona chahiye to now 
-  //  <div className='categories-container'>
-  //             {categories.map((category) => (
-  //                 <CategoryItem key={category.id} category={category} />
-  //             ))}
-  //  </div>
-  // isko utha ke iska bhi ek compoent banao 
-  // and directly us component ko yha render karao
-
-  return (
-    <>
-      {/* yha karo us component ko render */}
-    </>
-  );
+  return <Directory categories={categories} />;
 };
 
 export default App;
